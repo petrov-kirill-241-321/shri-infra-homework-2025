0fabcf541d09827ddbb2c874561f4e5da6861d9f
Kirill <kurumi8927@gmail.com>
2025-07-11 17:51:22 +0300

    change username in release.yml

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
       <div className="row">
         <div className="col">
           <h1>About</h1>
-          <p>Author: Kirilla</p>
+          <p>Author: Kirill</p>
         </div>
       </div>
     </div>
