58c78d722e7def8c6a366df1923d2bd47fa6285a
Kirill <kurumi8927@gmail.com>
2025-07-11 13:44:48 +0300

    проверка на работаспособность actions

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
       <div className="row">
         <div className="col">
           <h1>About</h1>
-          <p>Author: Kirill</p>
+          <p>Author: Kirilla</p>
         </div>
       </div>
     </div>
